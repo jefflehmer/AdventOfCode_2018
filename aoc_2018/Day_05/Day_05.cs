b26226221682e14f3c708a7a2a883ed5636451b6
Marcus Cole <kendo_dragon@yahoo.com>
2018-12-05 13:34:12 -0500

    Day 5 bones

---
+++ aoc_2018/Day_05/Day_05.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace aoc_2018
+{
+    public class Day_05
+    {
+        public static void Do()
+        {
+            //const string InputFile = @"..\..\..\Day_05\data\Day_05_test.aoc";
+            const string InputFile = @"..\..\..\Day_05\data\Day_05_input.aoc";
+
+            Do_1(InputFile);
+            Do_2(InputFile);
+        }
+
+        static void Do_1(string srcFile)
+        {
+            var lines = System.IO.File.ReadAllLines(srcFile);
+
+            Console.WriteLine($"Day 5.1: { 0 }");
+        }
+
+        static void Do_2(string srcFile)
+        {
+            var lines = System.IO.File.ReadAllLines(srcFile);
+
+            Console.WriteLine($"Day 5.2: { 0 }");
+        }
+    }
+}
