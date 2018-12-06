707f638d1e0b3f17036c4c3fd05c446d948e42f6
Marcus Cole <kendo_dragon@yahoo.com>
2018-12-06 17:30:06 -0500

    Day 5 done

---
+++ aoc_2018/Day_05/Day_05.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc_2018
+{
+    public class Day_05
+    {
+        public static void Do()
+        {
+            //const string InputFile = @"..\..\..\Day_05\data\Day_05_test.aoc";
+            const string InputFile = @"..\..\..\Day_05\data\Day_05_input.aoc";
+
+            Do_1(InputFile);
+            Do_2(InputFile);
+        }
+
+        static void Do_1(string srcFile)
+        {
+            var polymer = System.IO.File.ReadAllText(srcFile);
+            Console.WriteLine($"Day 5.1: { Reduce(polymer) }");
+        }
+
+        static void Do_2(string srcFile)
+        {
+            var polymer = System.IO.File.ReadAllText(srcFile);
+            Console.WriteLine($"Day 5.2: { (from c in "abcdefghijklmnopqrstuvwxyz" select Reduce(polymer, c)).Min() }");
+        }
+
+        static int Reduce(string polymer, char? charToSkip = null)
+        {
+            var stack = new Stack<char>(" "); // can't have empty stack
+
+            foreach (var mer in polymer)
+            {
+                var top = stack.Peek();
+                if (char.ToLower(mer) == charToSkip) continue;
+                if (top != mer && char.ToLower(mer) == char.ToLower(top))
+                    stack.Pop();
+                else
+                    stack.Push(mer);
+            }
+            return stack.Count - 1; // compensate for empty space used to initialize the stack
+        }
+    }
+
+    public static class Extensions
+    {
+        public static char ToLower(this char charBuilder, char c)
+        {
+            return c <= 'Z' ? (char) (c - 'A' + 'a') : c;
+        }
+    }
+}
